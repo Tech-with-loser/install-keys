754cfff6e246b26205578084408d67c23ab88a3d
Tech-With-loser <thekhangulfam6@gmail.com>
2021-02-07 08:30:03 +0530

    termux-keys

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#Script by Tech With Loser
+
+clear
+
+apt-get update
+
+apt-get upgrade -y
+
+
+clear
+
+
+                        
+echo -e " \e[1m  \e[92m========================================================"                    
+echo -e "\e[1m \e[32m
+___ ____ ____ _  _ _  _ _  _    _  _ ____ _   _ ____ 
+ |  |___ |__/ |\/| |  |  \/     |_/  |___  \_/  [__  
+ |  |___ |  \ |  | |__| _/\_    | \_ |___   |   ___] 
+                                                     
+"
+
+echo -e "\033[32m================YOUTUBE= Tech With Loser\e[92m==================="                                                                
+
+sleep 4
+
+clear
+
+echo -e "\e[96m======================================"
+echo""
+echo -e "\e[1m\e[91m YOUTUBE = Tech With Loser"
+echo ""
+echo -e "\e[1m\e[93m Script by Tech with Loser"
+echo""
+echo -e "\e[1m\e[95m pls subcribe Tech with loser"
+echo""
+echo -e "\e[35m======================================"
+echo""
+echo -e "			\e[92m======================"
+echo""
+echo -e "			\e[96mPlease wait few sec"
+echo""      
+echo -e "			\e[93m======================"
+          
+sleep 1
+
+echo "extra-keys = [['F1','F2','F3','F4','F5','F6','F7'],['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> ~/.termux/termux.properties && termux-reload-settings 
+
+echo""
+sleep 1
+echo -e "	\e[91m==========================================================================="
+echo""
+echo -e "	\e[32m your keys successfully installed ❤️😘 pls type exit  And repone termux"
+echo""        
+echo -e "	\e[96m==========================================================================="
---
+++ remove.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm  ~/.termux/termux.properties && termux-reload-settings
+
+sleep 1
+
+echo -e "\e[95m==============================================================="
+echo""
+echo -e "\e[96mYour keys successfully uninstalled please support love you all"
+echo""
+echo -e "=================================================================="
+             
